147012b4dd462b2c6668bef9762d089529b49aac
Leo <leandro.ola@gmail.com>
2024-09-01 13:53:36 +0200

    feat(home): Move title to a separate component

---
+++ src/views/home/components/Title/index.tsx
@@ -0,0 +1,10 @@
+export function Title() {
+  return (
+    <div className=" text-center py-16">
+      <h2 className="text-3xl md:text-5xl mb-2">Find your Oompa Loompa</h2>
+      <p className="text-2xl md:text-4xl text-gray-500">
+        There are more than 100k
+      </p>
+    </div>
+  )
+}
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,18 +1,14 @@
 import { OompaList } from './components/oompaList'
 import { SearchBar } from './components/searchBar'
 import { BaseLayout } from '@src/layouts/base'
+import { Title } from './components/Title'
 
 export default function Home() {
   return (
     <BaseLayout>
       <main className="px-20">
         <SearchBar />
-        <div className=" text-center py-16">
-          <h2 className="text-3xl md:text-5xl mb-2">Find your Oompa Loompa</h2>
-          <p className="text-2xl md:text-4xl text-gray-500">
-            There are more than 100k
-          </p>
-        </div>
+        <Title />
         <OompaList />
       </main>
     </BaseLayout>
